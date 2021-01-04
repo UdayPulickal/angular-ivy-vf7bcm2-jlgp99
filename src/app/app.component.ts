5e63148e4e0b52cb73cfa91f98b24123a48e427f
UdayPulickal <u_dayan@yahoo.co.in>
2021-01-04 00:59:16 +0000

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    Your Text : <input type="text" [(ngModel)]="userText" /> <br /><br />
-    <simple [simpleInput]="userText"></simple>
+    <list-employee></list-employee>
   `
 })
 export class AppComponent {
